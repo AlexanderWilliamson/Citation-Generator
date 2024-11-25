a5625659ba877b3ad31dc89c92ffb247c2854d1e
Alexander Williamson <160697078+AlexanderWilliamson@users.noreply.github.com>
2024-11-24 20:53:58 -0500

    Add files via upload
    Citations generated are now more accurate and have corrected formats

--- Citation-Generator/service-worker.js
+++ Citation-Generator/service-worker.js
@@ -1,12 +1,12 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	console.log("hi");
-	console.log(message.data);
+	//console.log(message.data);
 	if (message.type !== "website-fetcher-htmlpage")
 		return;
 	
 	let modifiedData = message.data.replace(/<script([^>]*)>.*?<\/script>/igs, "").replace(/<style([^>]*)>.*?<\/style>/igs, "").replace(/(<([^>]+)>)/igs, "").replace(/[\n\r]/g, "").replace(/(\s{2,})|(\t)/g, " ");
 	
-	console.log(modifiedData);
+	//console.log(modifiedData);
 	
 	let apiKey = "";
 	let requestType = "";
@@ -18,23 +18,24 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 			requestType = res["requested-formats"];
 		}
 		else{
-			sendResponse({"type": "service-worker-citation-result", "data": "Request to access chrome.storage.local failed, please try again"});
+			sendResponse({"type": "service-worker-citation-result", "error": "Request to access chrome.storage.local failed, please try again"});
+			return true;
 		}
 		
 		if (!apiKey){
-			sendResponse({"type": "service-worker-citation-result", "data": "Could not get apikey, click on the extension and enter your Google Gemini API key to run this extension"});
+			sendResponse({"type": "service-worker-citation-result", "error": "Could not get apikey, click on the extension and enter your Google Gemini API key to run this extension"});
 			return true;
 		}
 		
 		if (!requestType)
-			requestType = "am";
+			requestType = "ma";
 		
 		if (requestType == 'a')
-			request = `You will be given an html website with just its text, fully cite the website in APA format. Only respond in JSON, using the schema {apacitation:(yourCitation)}. Here is the link to the website ${message.link}. Here is the website. \"${modifiedData}\"`;
+			request = `You will be given an html website with just its text, fully cite the website in APA format. Remember in APA format the authors must be listed in ascending alphabetical order by last name then first name. When referencing a news article that doesn’t change, APA format no longer requires the retrieval date. Only respond in JSON, using the schema {apacitation:(yourCitation)}. Here is the link to the website ${message.link}. Here is the website. \"${modifiedData}\"`;
 		else if (requestType == 'm')
-			request = `You will be given an html website with just its text, fully cite the website in MLA format. You will use your best judgement to determine what type of resource you are citing. Only respond in JSON, using the schema {mlacitation:(yourCitation)}. Here is the link to the website ${message.link}. Here is the website. \"${modifiedData}\"`;
+			request = `You will be given an html website with just its text, fully cite the website in MLA format. You will use your best judgement to determine what type of resource you are citing. The titles in the MLA citation should use headline-style capitalization, and in MLA, when there are three or more authors the first author should be written as normal, then "et al." should follow it, along with the rest of the citation. Only respond in JSON, using the schema {mlacitation:(yourCitation)}. Here is the link to the website ${message.link}. Here is the website. \"${modifiedData}\"`;
 		else
-			request = `You will be given an html website with just its text, fully cite the website in both APA and MLA format. You will use your best judgement to determine what type of resource you are citing. Only respond in JSON, using the schema {apacitation:(yourAPACitation), mlacitation:(yourMLACitation)}. Here is the link to the website ${message.link}. Here is the website. \"${modifiedData}\"`;
+			request = `You will be given an html website with just its text, fully cite the website in both APA and MLA format. You will use your best judgement to determine what type of resource you are citing. For the APA format, the authors must be listed in ascending alphabetical order by last name then first name. When referencing a news article that doesn’t change, APA format no longer requires the retrieval date. The titles in the MLA citation should use headline-style capitalization, and in MLA, when there are three or more authors the first author should be written as normal, then "et al." should follow it, along with the rest of the citation. Only respond in JSON, using the schema {apacitation:(yourAPACitation), mlacitation:(yourMLACitation)}. Here is the link to the website ${message.link}. Here is the website. \"${modifiedData}\"`;
 		
 		const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
 		
@@ -48,7 +49,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 					{
 						"parts": [
 							{
-								"text": `You will be given an html website with just its text, fully cite the website in APA format. Only respond in JSON, using the schema {citation:(yourCitation)}. Here is the link to the website ${message.link}. Here is the website. \"${modifiedData}\"`
+								"text": request
 							}
 						]
 					}
@@ -61,11 +62,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 			}
 			return response.json();
 		}).then(data => {
-			console.log(data);
-			sendResponse({"type": "service-worker-citation-result", "data": data.candidates[0].content.parts[0].text});
+			console.log(data.candidates[0].content.parts[0].text.slice(data.candidates[0].content.parts[0].text.indexOf('{'), data.candidates[0].content.parts[0].text.lastIndexOf('}') + 1));
+			sendResponse({"type": "service-worker-citation-result", "data": JSON.parse(data.candidates[0].content.parts[0].text.slice(data.candidates[0].content.parts[0].text.indexOf('{'), data.candidates[0].content.parts[0].text.lastIndexOf('}') + 1))});
 		}).catch(error => {
 			console.log(error);
-			sendResponse({"type": "service-worker-citation-result", "data": error});
+			sendResponse({"type": "service-worker-citation-result", "error": error.message.replace(/,/g, "")});
 		});
 		return true;
 	});
--- Citation-Generator/website-fetcher.js
+++ Citation-Generator/website-fetcher.js
@@ -7,36 +7,72 @@ document.addEventListener("keyup", function(event) {
 	if (!websiteHTML)
 		return;
 	
+	if (!document.getElementById("iooigashoW$#Gdgsx-botdiv-websitecitationgeneratordiv")){
+		const div = document.createElement("div");
+		const topdiv = document.createElement("div");
+		const botdiv = document.createElement("div");
+		const btn = document.createElement("button");
+		const title = document.createElement("p");
+
+		div.style = "position:fixed; top:50px; right:0px; width:25em; max-width:25em; height:25em; overflow:auto; z-index:999; background-color:white; border:3px black solid;"
+
+		topdiv.innerHTML = "Citation Generator";
+		topdiv.style = "position:absolute; top:0px; width:98%; background-color:black; color:white; font-weight:bold; padding-left:2%";
+
+		botdiv.innerHTML = "Loading...";
+		botdiv.style = "position:absolute; top:20px; width:97%; white-space: normal; overflow-x:hidden; word-wrap: break-word; padding-left:2%";
+		botdiv.id = "iooigashoW$#Gdgsx-botdiv-websitecitationgeneratordiv";
+
+		btn.innerHTML = "X";
+		btn.style = "position:absolute; top:0px; right:0px; background:red; width:1.2em; height:1.2em; text-align:center; padding:0;";
+		btn.addEventListener("click", (evt) => {div.remove();});
+
+		topdiv.appendChild(btn);
+
+		div.appendChild(topdiv);
+		div.appendChild(botdiv);
+
+		document.body.appendChild(div);
+	}
+	else{
+		document.getElementById("iooigashoW$#Gdgsx-botdiv-websitecitationgeneratordiv").innerHTML = "Loading...";
+	}
+	
 	chrome.runtime.sendMessage({"type": "website-fetcher-htmlpage", "data": websiteHTML, "link": window.location.href}, (response) => {
-		if (document.getElementById("iooigashoW$#Gdgsx-botdiv-websitecitationgeneratordiv")){
-			document.getElementById("iooigashoW$#Gdgsx-botdiv-websitecitationgeneratordiv").innerHTML = response.data;
-		}
+		let finalResponse = "";
+		
+		if (response.error)
+			finalResponse = response.error;
 		else{
-			const div = document.createElement("div");
-			const topdiv = document.createElement("div");
-			const botdiv = document.createElement("div");
-			const btn = document.createElement("button");
-			const title = document.createElement("p");
-
-			div.style = "position:fixed; top:50px; right:0px; width:25em; max-width:25em; height:25em; overflow:auto; z-index:999; background-color:white; border:3px black solid;"
-
-			topdiv.innerHTML = "Text Summarizer";
-			topdiv.style = "position:absolute; top:0px; width:98%; background-color:black; color:white; font-weight:bold; padding-left:2%";
-
-			botdiv.innerHTML = response.data;
-			botdiv.style = "position:absolute; top:20px; width:97%; white-space: normal; overflow-x:hidden; word-wrap: break-word; padding-left:2%";
-			botdiv.id = "iooigashoW$#Gdgsx-botdiv-websitecitationgeneratordiv";
-
-			btn.innerHTML = "X";
-			btn.style = "position:absolute; top:0px; right:0px; background:red; width:1.2em; height:1.2em; text-align:center; padding:0;";
-			btn.addEventListener("click", (evt) => {div.remove();});
-
-			topdiv.appendChild(btn);
-
-			div.appendChild(topdiv);
-			div.appendChild(botdiv);
-
-			document.body.appendChild(div);
+			if (response.data.apacitation)
+				finalResponse += "APA: " + italicizeText(response.data.apacitation);
+				
+			if (response.data.mlacitation){
+				if (finalResponse)
+					finalResponse += "<br><br>";
+				finalResponse += "MLA: " + italicizeText(response.data.mlacitation);
+			}
 		}
+		if (!finalResponse)
+			finalResponse = "The model failed to properly format it's response, please try again."
+		
+		document.getElementById("iooigashoW$#Gdgsx-botdiv-websitecitationgeneratordiv").innerHTML = finalResponse;
 	});
 });
+
+function italicizeText(txt){
+	let insideAstericks = false;
+	let finalResult = "";
+	for (let i = 0; i < txt.length; i++){
+		if (txt[i] == '*'){
+			insideAstericks = !insideAstericks;
+			if (insideAstericks)
+				finalResult += "<i>";
+			else
+				finalResult += "</i>";
+		}
+		else
+			finalResult += txt[i];
+	}
+	return finalResult;
+}
